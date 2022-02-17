05618ca6525d961cd09324f93a13b9d5453f3a6b
Jose Lema <75354391+mauilema@users.noreply.github.com>
2022-02-16 21:49:24 -0500

    Create bubbleSortSolution.js

---
+++ bubbleSortSolution.js
@@ -0,0 +1,21 @@
+function bubbleSort(array) {
+	let sorted = false;
+	let lastElCounter = 0;
+	while (!sorted) {
+		sorted = true;
+		for (let i = 0; i < array.length-1-lastElCounter; i++) {
+			if (array[i] > array[i+1]){
+				swapEl(i,i+1,array);
+				sorted = false;
+			}	
+		}
+		lastElCounter++
+	}
+	return array;
+}
+
+function swapEl (i, j, array) {
+	[array[i], array[j]] = [array[j], array[i]]
+}
+
+<!-- Comments to come soon -->
