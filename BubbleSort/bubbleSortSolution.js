dee08bfa274a20446ccbac1bd93ed512087e33a6
mauilema <srjorgemaciel2002@gmail.com>
2022-02-18 16:35:08 -0500

    clarified some comments in the bubble sort solution js file

--- BubbleSort/bubbleSortSolution.js
+++ BubbleSort/bubbleSortSolution.js
@@ -7,11 +7,11 @@ function bubbleSort(array) {
 	//element in the array is in order. So we exclude that last element on the next for loop iteration by
 	//subtracting the accumulator from the array length
 	while (!sorted) {  //while !sorted is truthy run the code block inside the while loop
-		sorted = true; //changes sorted's value from false to true. Once the for all is done all iterations,
+		sorted = true; //changes sorted's value from false to true. Once all for loops iterations/swapping are done,
 		//this value will remain true and cause !sorted to be false which allows the code to conclude
 		for (let i = 0; i < array.length-1-lastElCounter; i++) { //loop through the array, stop at the last
 			//element in the array, subtract 1 from the array length after each full iteration
-			if (array[i] > array[i+1]){//if the value of the element is greater than the vulue of the element 
+			if (array[i] > array[i+1]){//if the value of the element is greater than the value of the element 
 				//next to it on it's right side, swap those elements
 				swapEl(i,i+1,array); //call back function that swaps 2 elements in the array
 				sorted = false;//set sorted to false so that the while loop can continue until all array 
@@ -20,10 +20,11 @@ function bubbleSort(array) {
 		}
 		lastElCounter++
 	}
-	return array;//once all iterations/swapping is done, return the final sorted array
+	return array;//once all iterations/swapping are done, return the final sorted array
 }
 
-function swapEl (i, j, array) {//sway array's elements using their index as arguments
+function swapEl (i, j, array) {//takes both indexes to be swapped and the array as arguments. Uses new ES6 syntax to swap 
+	//those elements in the array
 	[array[i], array[j]] = [array[j], array[i]]
 }
 
