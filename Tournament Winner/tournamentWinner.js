1d4480590d01086d0b8dc9ece971bf17195ba297
Jose Lema <75354391+mauilema@users.noreply.github.com>
2022-03-10 15:11:54 -0500

    Update tournamentWinner.js

--- Tournament Winner/tournamentWinner.js
+++ Tournament Winner/tournamentWinner.js
@@ -1,15 +1,18 @@
-const HOME_TEAM_WON = 1; //define a global variable to store a 1 to signify that the home team won
+const HOME_TEAM_WON = 1; //define a global variable to store a 1 which signifies that the home team won
 
 function tournamentWinner(competitions, results) {
-  // Write your code here.
-	let currentWinningTeam = "";// define a variable that will store an empty array and eventually 
+	let currentWinningTeam = "";// define a variable that will store an empty string and eventually 
 	//store the current team with the most points
-	const scoreCount = {[currentWinningTeam]: 0};//declare an object that will store the current
-	//winning team and it's score
+	
+	const scoreCount = {[currentWinningTeam]: 0};//declare an object that will store the all
+	//winning teams and their score, the obj starts with the current winning team key and 0 value so that there is a best
+	//team parameter. Eventually the winning teams will take it's place; depending on which winning team has the most points
+	
 	for (let i = 0; i < competitions.length; i++) {//iterate through the competions array
-		const result = results[i]//define a variable that will store the value of current index 
+		const result = results[i]//define a variable that will store the value of current index in the results array 
 		const [home, away] = competitions[i]; //deconstruct the variables home and away from the current
 		//value of the index in the competitions array
+		
 		const winningTeam = result === HOME_TEAM_WON ? home : away;//define a variable that will be either
 		//the home team or the away team depending on the value of the current index at the results array
 		
@@ -18,7 +21,7 @@ function tournamentWinner(competitions, results) {
 		if (scoreCount[winningTeam] > scoreCount[currentWinningTeam]) {//compare the value of the winning team
 			//key in the score count obj. If it is greater than the value of the current winning team at the 
 			//score count object...
-			currentWinningTeam = winningTeam; //... switch the current winning team with the winning team
+			currentWinningTeam = winningTeam; //... assign the value of the winning team to the current winning team
 		}
 		
 	}
@@ -29,5 +32,6 @@ function tournamentWinner(competitions, results) {
 function updateScores(team, points, scores) {
 	if (!(team in scores)) scores[team] = 0;//if the winning team is not in the score count object, 
 	//add a new key value pair where the winning team has a value of 0
-	scores[team] += points;//otherwise, add 3 points to the winning team in the score count object
+	
+	scores[team] += points;//then, add 3 points to the winning team in the score count object
 }
