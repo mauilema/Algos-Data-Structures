7661bccb967525d139dd825ec68503a85a6754c5
mauilema <srjorgemaciel2002@gmail.com>
2022-03-17 21:31:10 -0400

    added a new js solution file to the Find Closest Value in BST Algo. It includes a more efficient iterative approach

--- Find Closest Value In BST/findClosestValueInBST.js
+++ Find Closest Value In BST/findClosestValueInBST.js
@@ -44,11 +44,3 @@ function findClosestValueInBst(tree, target) {
           return closest;
       }
   }
-  // This is the class of the input tree. Do not edit.
-  class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-  }
