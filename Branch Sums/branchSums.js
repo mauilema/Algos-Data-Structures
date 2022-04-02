5f97d404cee5a8393e862e43017e7b565a127994
mauilema <srjorgemaciel2002@gmail.com>
2022-04-01 21:26:19 -0400

    added the solution js file for Branch Sums Algo

--- Branch Sums/branchSums.js
+++ Branch Sums/branchSums.js
@@ -1,1 +1,31 @@
-///To be continued...
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  function branchSums(root) {
+    const sums = [];
+      computeBranchSums(root, 0, sums);
+      return sums;
+  }
+  
+  function computeBranchSums(node, runningSum, sums) {
+      if (!node) return;
+      
+      const newRunningSum = runningSum + node.value;
+      if (!node.left && !node.right) {
+          sums.push(newRunningSum);
+          return;
+      }
+      
+      computeBranchSums(node.left, newRunningSum, sums);
+      computeBranchSums(node.right, newRunningSum, sums);
+  }
+  
+
+  
