ffc23b5ab466a422c42551a6b061f46b20fecb15
Jose Lema <75354391+mauilema@users.noreply.github.com>
2022-04-02 21:49:26 -0400

    Update branchSums.js
    updated the position of some comments for an improved readability

--- Branch Sums/branchSums.js
+++ Branch Sums/branchSums.js
@@ -11,31 +11,29 @@ function branchSums(root) {
   const sums = []; //declare a const variable that stores an empty array. Branch sums will eventually 
 									// be added to this array.
 	
-	computeBranchSums(root, 0, sums);//call a helper function with the root, 0 value runningSum and the 
-																	//sums array as arguments. 
+	computeBranchSums(root, 0, sums);//call a helper function with the root, 0 value runningSum and the sums array as arguments. 
 	
-	return sums;//Once all branch sums have been completed, return the sums array containing all branch
-							//sum values. 
+	return sums;//Once all branch sums have been completed, return the sums array containing all branch sum values. 
 }
 
 function computeBranchSums(node, runningSum, sums) {
-	if (!node) return;//if we get to a node that has only a left or only a right child node, return at that missing
-										//node. 
+	if (!node) return;//if we get to a node that has only a left or only a right child node, return at that missing node.
+										
 	
-	const newRunningSum = runningSum + node.value;//declare a const variable that stores the current running sum plus
-																								//the current node value.
+	const newRunningSum = runningSum + node.value;//declare a const variable that stores the current running sum plus the current node value.
+															
 	
-	if (!node.left && !node.right) {//declare a conditional that checks if a node has no left and no right child nodes,
-																	//then it must be a leaf.
+	if (!node.left && !node.right) {//declare a conditional that checks if a node has no left and no right child nodes, then it must be a leaf.
+																	
 		sums.push(newRunningSum);//since we have hit a leaf, push the branch sum to the sums array and return;
 		return;
 	}
 	
 	computeBranchSums(node.left, newRunningSum, sums);//recursively call the computeBranchSums function with the left node 
-	//value, newRunninSum and sums array as arguments. 
+	//value, newRunningSum and sums array as arguments. 
 	
 	computeBranchSums(node.right, newRunningSum, sums);//recursively call the computeBranchSums function with the right node 
-	//value, newRunninSum and sums array as arguments. 
+	//value, newRunningSum and sums array as arguments. 
 }
 
 
