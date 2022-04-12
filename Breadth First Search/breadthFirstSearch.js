f69d9336d56bcc3adcb80810d6f7a9ba5c42fa52
Jose Lema <75354391+mauilema@users.noreply.github.com>
2022-04-11 23:44:38 -0400

    Update breadthFirstSearch.js
    updated the solution js file for Breadth First Search. Applied necessary grammar/spelling corrections on the comments.

--- Breadth First Search/breadthFirstSearch.js
+++ Breadth First Search/breadthFirstSearch.js
@@ -14,18 +14,18 @@ class Node {
        const queue = [this]; //declare a const variable "queue" that stores the root node. This array will serve as a queue where the first node in is the first node out; aka FIFO.
        
        while (queue.length > 0) {//use a while loop to traverse the queue while it is not empty. Node's will be added into the queue, starting from the root node. Then it's
-           //children from left to right. 
+           //children from left to right, top to bottom (level by level). 
            
        const currentNode = queue.shift(); //declare a const variable "currentNode". It will store the current node that was just shifted from the left beginning of the array. The shift
-           //method is used to get the effect of a queue FIFO mechanism.
+           //method is used to get the FIFO effect of a queue.
   
        array.push(currentNode.name); //push the name of the current node (which was just shifted off of the queue) into the original input array. 
        
       
-       for (const child of currentNode.children) { //use a for off loop to access each child belonging to the current node (the one just shifted off of the queue). currentNode.children
+       for (const child of currentNode.children) { //use a for of loop to access each child belonging to the current node (the one just shifted off of the queue). currentNode.children
            //is an array itself. That is why we use a for of loop to access it's elements which are objects. We are only interested in the value of those elements. 
            
-           queue.push(child) //push each of the current node's child into the queue. The leftmost child will be shifted off of the array in the next step which is the first line
+           queue.push(child) //push each of the current node's child into the queue. The leftmost child will be shifted off of the array in the next step which is the first line of code
            //in this while loop. 
        }	 
        }
