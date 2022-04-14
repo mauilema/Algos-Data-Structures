e39c8e1349d45df9cb9c96a28f17caaf82e3ecce
mauilema <srjorgemaciel2002@gmail.com>
2022-04-13 23:35:43 -0400

    added more comments on the js solution file for the Binary Tree Diameter ALgo

--- Binary Tree Diameter/binaryTreeDiameter.js
+++ Binary Tree Diameter/binaryTreeDiameter.js
@@ -25,12 +25,21 @@ class BinaryTree {
       const leftTreeInfo = getTreeInfo(tree.left); //declare a const variable "leftTreeInfo" that stores the result of make a recursive call with the node's left child. 
       const rightTreeInfo = getTreeInfo(tree.right); //declare a const variable "rightTreeInfo" that stores the result of make a recursive call with the node's right child. 
       
-      const longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height; //declare a const variable "longestPathThroughRoot" that stores the value of adding the current node's left child height plus its right child height.
-      const maxDiameterSoFar = Math.max(leftTreeInfo.diameter, rightTreeInfo.diameter); //declare a const variable "maxDiameterSoFar" that store the maximum of a node's left child diameter vs its right child diameter 
-      const currentDiameter = Math.max(longestPathThroughRoot, maxDiameterSoFar); //declare a const variable "currentDiameter"  that stores the maximum of the longestPathThroughRoot vs maxDiameterSoFar.
-      const currentHeight = 1 + Math.max(leftTreeInfo.height, rightTreeInfo.height); //declare a const variable "current Height" that adds 1 to the maximum value between a node's left child height and its right child height. 
       
-      return new TreeInfo(currentDiameter, currentHeight) //once all the recursive calls have ran, return a new object "TreeInfo" with the last current Diameter and current Height as properties. We will be accessing
+      const longestPathThroughRoot = leftTreeInfo.height + rightTreeInfo.height; //declare a const variable "longestPathThroughRoot" that stores the value of adding the current node's left child height plus its right child height.
+      //The longest path through a root should include the nodes on both sides of it's children. No matter how large one side of it's branch is, adding the other side will increase the total length of this path. 
+      
+      const maxDiameterSoFar = Math.max(leftTreeInfo.diameter, rightTreeInfo.diameter); //declare a const variable "maxDiameterSoFar" that store the maximum of a node's left child diameter vs its right child diameter 
+      //The max diameter depends on how many nodes there are on each branch. The more nodes, the more edges. More edges increases the size of the diameter. 
+
+      const currentDiameter = Math.max(longestPathThroughRoot, maxDiameterSoFar); //declare a const variable "currentDiameter"  that stores the maximum of the longestPathThroughRoot vs maxDiameterSoFar.
+      //current diameter can be either of the 2 parameters in the max method because a node can have more child nodes on one of it's branches or more child nodes on a combination between both branches.
+    
+      const currentHeight = 1 + Math.max(leftTreeInfo.height, rightTreeInfo.height); //declare a const variable "current Height" that adds 1 to the maximum value between a node's left child height and its right child height. 
+      //the reason why you choose between both side's height is because we want the height that is from the branch that is lower in the binary tree. This translates to the greater of the heights which we need to 
+      //calculate a node's height. Once we have the largest height between it's children, we simply add 1 because we're going up a level in the tree. 
+
+      return new TreeInfo(currentDiameter, currentHeight) //once all the recursive calls have ran, return a new instance of "TreeInfo" with the last current Diameter and current Height as properties. We will be accessing
       //the diameter value from this object. 
   }
   
